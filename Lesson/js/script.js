162a9bb004b143f3a468610d3969486f55998aaa
Ilnytskyy Markiian <ilnytskyymarkiian@gmail.com>
2026-03-26 14:16:02 +0200

    save progress

--- Lesson/js/script.js
+++ Lesson/js/script.js
@@ -1,145 +1,167 @@
-// процедурне програмування
-
-// const summ = 20000;
+// const sum = 20000;
 // const month = 12;
 // const p = 1000;
 
 // const credit = (sum, date, p) => {
-//   return sum + p * date;
+//     return sum + p * date;
 // };
 
-// console.log(credit(summ, month, p));
+// console.log(credit(sum, month, p));
 
-// об*єктно орієнтоване програмування
+////////////////////////////////////////////////////////////////
 
 // const credit = {
-//   summ: 20000,
-//   month: 12,
-//   p: 1000,
-//   result() {
-//     return this.summ + this.p * this.month;
-//   },
+//     sum: 20000,
+//     month: 12,
+//     p: 1000,
+//     result() {
+//         return this.sum + this.p * this.month;
+//     }
 // };
 
 // console.log(credit.result());
 
-//класи
+////////////////////////////////////////////////////////////////
 
-// class Bank {
-//   static type = "Privat";
-//   constructor(options) {
-//     this.summ = options.summ;
-//     this.month = options.month;
-//     this.p = options.p;
-//   }
+// class Banc {
+//     static type = "PrivatBank";
 
-//   credit() {
-//     return console.log("I am Privat");
-//   }
+//     constructor(options) {
+//         this.sum = options.sum;
+//         this.month = options.month;
+//         this.p = options.p;
+//     }
+
+//     credit() {
+//         return console.log("I am Privat");
+//     }
 // }
 
-// const userBank = new Bank({
-//   summ: 30000,
-//   month: 5,
-//   p: 500,
-// });
+// // const userBank = new Banc({
+// //     sum: 30000,
+// //     month: 5,
+// //     p: 500,
+// // });
 
-// userBank.credit();
-// console.log(Bank.type);
+// // userBank.credit();
+// // console.log(Banc.type);
 
-// class NewBank extends Bank {
-//   constructor(options) {
-//     super(options);
-//     this.card = options.card;
-//   }
-//   credit() {
-//     super.credit();
-//     return console.log("I am Aval");
-//   }
+// class newBank extends Banc {
+//     constructor(options) {
+//         super(options);
+//         this.card = options.card;
+//     }
+//     credit() {
+//         super.credit();
+//         console.log("I am Aval");
+//     }
 // }
 
-// const aval = new NewBank({
-//   summ: 30000,
-//   month: 5,
-//   p: 500,
-//   card: true,
+// const newBankObj = new newBank({
+//     sum: 30000,
+//     month: 5,
+//     p: 500,
+//     card: true,
 // });
 
-// get & set
+// console.log(newBankObj);
+// newBankObj.credit();
+
+////////////////////////////////////////////////////////////////
 
 // class User {
-//   constructor(props) {
-//     this.name = props.name;
-//   }
+//     constructor(props) {
+//         this.name = props.name;
+//     }
 
-//   firstName = "";
-//   lastName = "";
-//   age = "";
-//   city = "";
+//     firstName = "";
+//     lastName = "";
+//     age = NaN;
+//     city = "";
 
-//   set name(newName) {
-//     const nameRow = newName.split(" ");
-//     this.firstName = nameRow[0];
-//     this.lastName = nameRow[1];
-//   }
+//     set name(newName) {
+//         const nameRow = newName.split(" ");
+//         this.firstName = nameRow[0];
+//         this.lastName = nameRow[1];
+//     }
 
-//   get name() {
-//     return ` First name : ${this.firstName} last name : ${this.lastName}`;
-//   }
+//     get name() {
+//         return `First name: ${this.firstName}. Last name: ${this.lastName}.`
+//     }
 // }
 
 // const Kris = new User({
-//   name: "Kristina Malitowska",
+//     name: "Kristina Malitowska",
 // });
 
 // const Dasha = new User({
-//   name: "Dasha Happy",
+//     name: "Dasha Happy",
 // });
 
+// console.log(Kris);
 // console.log(Dasha);
 
+////////////////////////////////////////////////////////////////
+
 // const a = [1, 2, 3];
+
 // console.log(a);
 
+////////////////////////////////////////////////////////////////
+
 // const a = {
-//   x: 1,
-//   y: 2,
+//     x: 1,
+//     y: 2,
 // };
 
 // const b = Object.create(a);
 
-// console.log(b);
+// console.log(b.y);
 
-// const Manager = function (name, sales) {
-//   this.name = name;
-//   this.sales = sales;
+////////////////////////////////////////////////////////////////
+
+// const Manager = function(name, sales) {
+//     this.name = name;
+//     this.sales = sales;
 // };
 
 // const ann = new Manager("Anna", 5);
 // const ivan = new Manager("Ivan", 10);
 
-// Manager.prototype.sale = function () {
-//   this.sales += 1;
-// };
+// Manager.prototype.sale = function() {
+//     this.sales += 1;
+// }
+
+// // console.log(ann);
+// // ann.sale();
+// // console.log(ann);
 
 // console.dir(Manager);
 
+////////////////////////////////////////////////////////////////
+
 // class CoffeMachine {
-//   _water = 0;
-//   #waterLimit = 500;
+//     _water = 0;
 
-//   constructor(power) {
-//     this.power = power;
-//   }
+//     #waterLimit = 500;
 
-//   set waterAmount(value) {
-//     if (value < 0) {
-//       value = 0;
+//     constructor(power) {
+//         this.power = power;
 //     }
-//     this._water = value;
-//   }
+
+//     set waterAmount(value) {
+//         if(value < 0) {
+//             value = 0;
+//         }
+
+//         this._water = value;
+//     }
 // }
 
-// let coffeeMachine = new CoffeMachine(100);
+// let coffeMachine = new CoffeMachine(100);
 
-// coffeeMachine.waterAmount = -20;
+// coffeMachine.waterAmount = -20;
+
+// console.log(coffeMachine);
+
+////////////////////////////////////////////////////////////////
